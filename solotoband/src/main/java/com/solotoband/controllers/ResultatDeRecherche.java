1c83aa24094a3192532894f924095ab81da5a333
ysp <54001533+ysp313@users.noreply.github.com>
2019-11-15 16:14:44 +0200

    Modifications suite au pull request

--- solotoband/src/main/java/com/solotoband/controllers/ResultatDeRecherche.java
+++ solotoband/src/main/java/com/solotoband/controllers/ResultatDeRecherche.java
@@ -1,4 +1,4 @@
-package com.solotoband.solotoband.controllers;
+package com.solotoband.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
